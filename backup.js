2877ab90cb3b02917027bf433cc4fba6f01fcb7b
Mat Harden <matharden@gmail.com>
2017-01-18 21:57:41 +0000

    Working

--- backup.js
+++ backup.js
@@ -1,31 +1,34 @@
 var Nightmare = require('nightmare');
-var nightmare = new Nightmare();
+var nightmare = new Nightmare(
+  {show:true});
 var fs = require('fs');
 
 nightmare
   .goto('https://workflowy.com/accounts/login/')
 
   // login
-  .type('#id_username', process.env.USERNAME)
-  .type('#id_password', process.env.PASSWORD)
+  .type('#id_username', 'matharden@gmail.com')
+  .type('#id_password', 'lego18lists')
   .click('[type=submit]')
-  .wait()
+  .wait(5000)
 
   // open export panel
   .click('#exportAllButton')
-  .wait('.id_text')
+  .wait(500)
   .click('#id_text')
   .wait(500)
 
   // scrape content
-  .evaluate(function () {
+  .evaluate(function(){
     return document.querySelector('.previewWindow pre').innerText;
-  }, function (exportText) {
+  })
+  .end()
+  .then(function (exportText) {
     var filename = 'workflowy_export.txt';
     console.log('Saving export to ' + filename);
     fs.writeFile(filename, exportText);
   })
 
-  .run(function(err, nightmare){
-    console.log('Done.');
+  .catch(function(err){
+    console.log(err ? err : 'Done.');
   });
